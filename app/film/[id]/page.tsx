de2a7a3ba6c0417d95285ce6c0bce8da7f9f291a
ilSPRITEli <gvkgodvilekung@gmail.com>
2024-10-26 19:51:44 +0700

    film page update

--- app/film/[id]/page.tsx
+++ app/film/[id]/page.tsx
@@ -4,6 +4,24 @@ import React from "react";
 // import clock icon
 import { ClockIcon } from '@heroicons/react/24/outline';
 import { Button } from "@/components/ui/button";
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/components/ui/tabs"
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 interface Params {
   id: string;
@@ -13,9 +31,44 @@ export default async function Page({ params }: { params: Params }) {
 
   const movie = await getMovieById(Number(params.id));
 
+  const cast = [
+    {
+      name: 'John Doe',
+      image: '/actor.jpg'
+    },
+    {
+      name: 'Jane Doe',
+      image: '/actor.jpg'
+    },
+    {
+      name: 'John Doe',
+      image: '/actor.jpg'
+    },
+    {
+      name: 'Jane Doe',
+      image: '/actor.jpg'
+    },
+    {
+      name: 'John Doe',
+      image: '/actor.jpg'
+    },
+    {
+      name: 'John Doe',
+      image: '/actor.jpg'
+    },
+    {
+      name: 'John Doe',
+      image: '/actor.jpg'
+    },
+    {
+      name: 'John Doe',
+      image: '/actor.jpg'
+    },
+  ]
+
   return (
-    <main className="main_gradient flex flex-wrap grow flex-col items-start w-full h-auto gap-10 md:px-20 md:py-24 py-16 px-1 text-white">
-      <div className="flex flex-col md:flex-row w-fit h-96 gap-5">
+    <main className="main_gradient flex flex-wrap grow flex-col items-start w-full h-auto gap-5 md:px-20 md:pt-24 pt-16 md:pb-10 pb-10 px-1 text-white">
+      <div className="flex flex-col md:flex-row w-fit h-fit gap-5">
         <img className="object-cover w-full md:w-80 poster" src={'https://image.tmdb.org/t/p/original' + movie?.image ?? undefined} alt={movie?.title} />
         <div className="flex flex-col gap-5 justify-between">
           <div className="flex flex-col gap-5">
@@ -35,6 +88,30 @@ export default async function Page({ params }: { params: Params }) {
           <Button className="bg-amber-400 text-black md:w-1/2 hover:bg-amber-800">Watch Now</Button>
         </div>
       </div>
+      <Tabs defaultValue="overview" className="w-1/2 rounded-none">
+      <TabsList className="grid w-fit grid-cols-2 rounded-none bg-transparent">
+        <TabsTrigger className="w-fit px-5 text-base data-[state=active]:text-red-400 data-[state=active]:bg-transparent text-white/50" value="overview">Overview</TabsTrigger>
+        <TabsTrigger className="-fit px-5 text-base data-[state=active]:text-red-400 data-[state=active]:bg-transparent text-white/50" value="screens">Screens</TabsTrigger>
+      </TabsList>
+      <TabsContent value="overview">
+        {/* show cast */}
+        <Card className="w-full bg-transparent border-none shadow-none">
+          <CardHeader>
+            <CardTitle className="text-white">Cast</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <div className="grid grid-cols-3 md:grid-cols-8 gap-4">
+              {cast.map((cast: any) => (
+                <div key={cast.name} className="flex flex-col gap-2">
+                  <img className="object-cover w-24 h-24 md:w-full md:h-full aspect-square rounded-xl" src={cast.image} alt={cast.name} />
+                  <p className="text-white text-xs">{cast.name}</p>
+                </div>
+              ))}
+            </div>
+          </CardContent>
+        </Card>
+      </TabsContent>
+      </Tabs>
     </main>
   );
 }
